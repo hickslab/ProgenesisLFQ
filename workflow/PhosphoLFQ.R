6ef330e07e6f612f20bed2148c7fe254e11101a8
ewmcc <ewmcconnell@gmail.com>
2019-07-21 20:33:06 -0400

    Update PhosphoLFQ workflow

--- workflow/PhosphoLFQ.R
+++ workflow/PhosphoLFQ.R
@@ -1,17 +1,20 @@
-# PhosphoLFQ --------------------------------------------------------------
+# PhosphoLFQ
 # Phosphosite-level label-free quantification workflow
 
 
-# Load packages
+# ProcessLFQ ----
+
+
+# Packages
 library(Biostrings)
 library(devtools)
 library(tidyverse)
 
 
 # Or install packages
+#install.packages("BiocManager"); BiocManager::install("Biostrings")
 #install.packages("devtools")
 #install.packages("tidyverse")
-#install.packages("BiocManager"); BiocManager::install("Biostrings")
 
 
 # Set the working directory.
@@ -19,32 +22,32 @@ library(tidyverse)
 setwd("~/ProgenesisLFQ/data") # ???
 
 
-# Load functions
+# Functions
 url <- "https://raw.githubusercontent.com/hickslab/ProgenesisLFQ/master/"
 source_url(paste0(url, "R/ProcessLFQ.R"))
 
 
 # Load Peptide Measurements spreadsheet exported from Progenesis.
-pepm <- "20180513_EWM_QP1_Phos_5600_pepm.csv" %>% #???
+pepm <- "20190715_EWM_TOR1_Phospho_pepm.csv" %>% # ???
   read_csv(., skip = 2, col_types = cols())
 
 
 # Load Peptide Measurements spreadsheet exported from Progenesis.
-protm <- "20180513_EWM_QP1_Phos_5600_protm.csv" %>% #???
+protm <- "20190715_EWM_TOR1_Phospho_protm.csv" %>% # ???
   read_csv(., skip = 2, col_types = cols()) %>%
   select(1:4) %>%
   separate_rows(., Accession, sep = ";")
 
 
 # Load protein sequence database.
-database <- "Cr_v5.5_mt_chl_crap_20160502.fasta" %>% # ???
+database <- "Cr_uniprot_crap_20190130.fasta" %>% # ???
   readAAStringSet(.)
 
 names(database) <- str_split(names(database), " ", simplify = TRUE)[, 1]
 
 
 # Define normalized abundance columns in pepm
-samples <- 17:18 # ???
+samples <- 19:38 # ???
 
 
 # Process data
@@ -61,4 +64,108 @@ data <- pepm %>%
 
 
 # Write parsed data to file
-#write_csv(data, "###_processed.csv")
+#write_csv(data, "###_processed.csv") # ???
+
+
+# StatLFQ ----
+
+
+# Packages
+library(imp4p)
+library(broom)
+
+
+# Functions
+url <- "https://raw.githubusercontent.com/hickslab/ProgenesisLFQ/master/"
+source_url(paste0(url, "R/StatLFQ.R"))
+
+
+# Workflow
+# Define the column indeces for replicates in each condition.
+a <- 2:6; b <- 7:11; c <- 12:16; d <- 17:21 # ???
+
+group <- list("Control" = a, "AZD8055" = b, "Torin1" = c, "Rapamycin" = d) # ???
+
+group.compare <- list("Control-AZD8055" = list(a, b),
+                      "Control-Torin1" = list(a, c),
+                      "Control-Rapamycin" = list(a, d)) # ???
+
+
+# Rename the abundance columns in a simplified "Condition-Replicate" format.
+data <- data %>%
+  rename_columns(., group)
+
+
+# Clean, transform, and impute abundance columns
+data2 <- data %>%
+  clean_min(., group, nonzero = 3) %>% # ???
+  transform_data(., group, method = "log2") %>%
+  impute_imp4p(., group)
+
+
+# Pairwise *t*-test
+data3 <- data2 %>%
+  calculate_ttest(., group.compare, fdr = FALSE)
+
+
+# One-way ANOVA
+data4 <- data2 %>%
+  calculate_1anova()
+
+
+# Fold change
+data3 <- data3 %>%
+  calculate_fc(., group.compare, difference = TRUE) %>%
+  add_fc_max()
+
+data4 <- data4 %>%
+  calculate_fc(., group.compare, difference = TRUE) %>%
+  add_fc_max()
+
+
+# AnnotateLFQ ----
+
+
+# Functions
+url <- "https://raw.githubusercontent.com/hickslab/ProgenesisLFQ/master/"
+source_url(paste0(url, "R/AnnotateLFQ.R"))
+
+
+# UniProt
+data5 <- data4 %>%
+  add_missingness(., data, group)
+
+
+# PlotLFQ ----
+
+
+# Functions
+url <- "https://raw.githubusercontent.com/hickslab/ProgenesisLFQ/master/"
+source_url(paste0(url, "R/PlotLFQ.R"))
+
+
+# PCA
+# Principal component analysis (PCA)
+data2 %>% plot_pca(., group) +
+  theme_custom()
+
+
+# Volcano Plot
+data3 %>%
+  plot_volcano(.,
+               group,
+               group.compare,
+               fdr = FALSE,
+               threshold = 2,
+               xlimit = 8,
+               ylimit = 5) +
+  theme_custom()
+
+
+# Trend Profiles
+data4 %>%
+  filter(FDR < 0.05) %>% # ???
+  plot_hclust(., group, k = 2) +
+  theme_custom()
+
+
